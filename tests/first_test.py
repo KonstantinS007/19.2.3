41095ba019424e8be94f363a454692dfbec21a0e
KonstantinS007 <uzerovalex9@gmail.com>
2023-02-08 13:38:50 +0300

    Test 4

--- tests/first_test.py
+++ tests/first_test.py
@@ -9,15 +9,11 @@ class TestCalc:
     def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 6, 4) == 24
 
-
-
     def test_division_calculate_correctly(self):
         assert self.calc.division(self, 81, 9) == 9
 
-
     def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 35, 6) == 29
-
 
     def test_adding_calculate_correctly(self):
         assert self.calc.adding(self, 41, 33) == 74
